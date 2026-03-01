bc95d693a83a5cec2a6d2b2d3738956d4e2cd684
xOmilk <leite46052@gmail.com>
2026-03-01 19:03:57 -0300

    Add React Query and update API routes for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Toaster } from 'react-hot-toast';
 import '../src/css/App.css';
 import { AppRouter } from './routes/AppRouter';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
         <>
             <Toaster position="bottom-right" />
-            <AppRouter />
+            <QueryClientProvider client={queryClient}>
+                <AppRouter />
+            </QueryClientProvider>
         </>
     );
 }
--- src/constants/apiRoutesName.ts
+++ src/constants/apiRoutesName.ts
@@ -1,6 +1,17 @@
 const apiRoutesName = {
+    auth: {
+        login: '/login',
+        register: '/signup',
+        recoverPassword: '/recover-password',
+        resetPassword: '/reset-passowrd',
+    },
+
     events: {
-        getAllEvents: '',
+        getAllEvents: '/events',
+        getOneEvent: (idEvent: number) => `/events/${idEvent}`,
+        create: '/events',
+        update: (id: number) => `/events/${id}`,
+        delete: (id: number) => `/events/${id}`,
     },
 };
 
