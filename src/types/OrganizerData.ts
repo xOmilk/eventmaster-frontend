1997ac580f7a1dd1de4242c9296790afd07f421c
xOmilk <leite46052@gmail.com>
2026-03-25 22:50:19 -0300

    feat: add endpoint to retrieve all organizers and update OrganizerData type

--- src/types/OrganizerData.ts
+++ src/types/OrganizerData.ts
@@ -3,6 +3,7 @@ export type statusOrganizerRequest = 'rejected' | 'approved' | 'pending';
 export type OrganizerData = {
     id: number;
     name: string;
+    cpf: string;
     email: string;
     phone_number: string;
     reason: string;
